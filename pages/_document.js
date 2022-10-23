4d16040411a307d0cae10df42d0a7e1bcfcee8c7
dev-mahir <mdmahir.ds@gmail.com>
2022-10-23 17:23:17 +0600

    first commit

---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+// pages/_document.js
+
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link
+                    rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    crossorigin="true"
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700;800;900&family=Work+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap"
+                    rel="stylesheet"
+                />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+}
